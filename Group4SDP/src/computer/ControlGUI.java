4caa8ff79d929e84b707b1611212a000e4c8f2f8
Marija Pinkute <s1133718@corran.inf.ed.ac.uk>
2013-01-22 11:11:51 +0000

    All in one project

---
+++ Group4SDP/src/computer/ControlGUI.java
@@ -0,0 +1,140 @@
+package computer;
+
+import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.ByteBuffer;
+import java.io.*;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+import vision.VideoFeed;
+
+import lejos.pc.comm.NXTCommFactory;
+import lejos.pc.comm.NXTInfo;
+import communication.BluetoothCommunication;
+
+public class ControlGUI extends JFrame {
+	private JFrame frame = new JFrame("Control Panel");
+
+	private JPanel startPnl = new JPanel();
+	private JPanel kickPnl = new JPanel();
+	private JPanel stopPnl = new JPanel();
+
+	private JButton start = new JButton("Start");
+	private JButton kick = new JButton("Kick");
+	private JButton stop = new JButton("Stop");
+
+	// Communication variables
+	private static BluetoothCommunication comms;
+	
+	public static final String NXT_MAC_ADDRESS = "00:16:53:0A:07:1D";
+	public static final String NXT_NAME = "4s";
+
+	
+	// static Robot r = new Robot();
+
+	public static void main(String[] args) throws IOException {
+		   new VideoFeed();
+		// Sets up the gui
+		ControlGUI gui = new ControlGUI();
+		gui.Launch();
+		gui.action();
+
+		// Setting up the communication
+		comms = new BluetoothCommunication(NXT_NAME, NXT_MAC_ADDRESS);
+		comms.openBluetoothConnection();
+
+		// Testing the communication
+		//String tst = "";
+		//BufferedReader inn = new BufferedReader(new InputStreamReader(System.in));
+		//tst = inn.readLine();
+		//int command = Integer.parseInt(tst);
+		//byte[] bytes = ByteBuffer.allocate(4).putInt(Integer.parseInt(tst)).array();
+		while (!comms.isRobotReady()){};
+		System.out.println("Robot ready!");
+		int [] command = new int [] {66,0,0,66};
+		comms.sendToRobot(command);
+		int[] test = new int[4];
+		test = comms.receiveFromRobot();
+		System.out.println(test.toString());
+
+		// r.startCommunications();
+		// r.setConnected(false);
+	}
+
+	public ControlGUI() {
+		startPnl.add(start);
+		kickPnl.add(kick);
+		stopPnl.add(stop);
+		frame.getContentPane().setLayout(new BorderLayout());
+		frame.getContentPane().add(startPnl, BorderLayout.WEST);
+		frame.getContentPane().add(kickPnl, BorderLayout.CENTER);
+		frame.getContentPane().add(stop, BorderLayout.EAST);
+		frame.addWindowListener(new ListenCloseWdw());
+
+	}
+
+	public void action() {
+		start.addActionListener(new ActionListener() {
+
+			public void actionPerformed(ActionEvent e) {
+				System.out.println("Start...");
+				// r.moveForward(60);
+				// r.each_wheel_speed(-900, -750);
+				// r.accelerateRobot(100);
+				// try {
+				// Thread.sleep(2*1000);
+				// } catch (InterruptedException e1) {
+				// // TODO Auto-generated catch block
+				// e1.printStackTrace();
+				// }
+				// r.moveBackward(20);
+
+				// r.each_wheel_speed(50, 100);
+				// r.moveForwardByDistance(-60, 40);
+				// r.accelerateRobot(50);
+
+				// r.rotateRobot(90);
+
+			}
+
+		});
+
+		kick.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				System.out.println("Kick...");
+				// r.kick();
+
+			}
+		});
+
+		stop.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				System.out.println("Stop...");
+				// r.stop();
+			}
+		});
+
+	}
+
+	public class ListenCloseWdw extends WindowAdapter {
+		public void windowClosing(WindowEvent e) {
+			System.exit(0);
+		}
+	}
+
+	public void Launch() {
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.pack();
+		frame.setVisible(true);
+	}
+
+}
---
+++ Group4SDP/src/vision/VideoFeed.java
@@ -0,0 +1,136 @@
+package vision;
+
+import java.awt.Frame;
+import java.awt.Graphics;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.awt.image.BufferedImage;
+import java.util.List;
+
+import au.edu.jcu.v4l4j.CaptureCallback;
+import au.edu.jcu.v4l4j.Control;
+import au.edu.jcu.v4l4j.DeviceInfo;
+import au.edu.jcu.v4l4j.FrameGrabber;
+import au.edu.jcu.v4l4j.ImageFormat;
+import au.edu.jcu.v4l4j.V4L4JConstants;
+import au.edu.jcu.v4l4j.VideoDevice;
+import au.edu.jcu.v4l4j.VideoFrame;
+import au.edu.jcu.v4l4j.exceptions.ImageFormatException;
+import au.edu.jcu.v4l4j.exceptions.V4L4JException;
+
+public class VideoFeed extends Frame implements Runnable {
+	private static final int SATURATION = 50;
+	private static final int BRIGHTNESS = 200;
+	private static final int CONTRAST = 127;
+	private static final int HUE = 0;
+	private static final int CHROMA_GAIN = 0;
+	private static final int CHROMA_AGC = 1;
+
+    BufferedImage image = new BufferedImage(640, 480, BufferedImage.TYPE_INT_RGB);
+	VideoDevice videoDev;
+    FrameGrabber frameGrabber;
+    
+    Thread animation;
+    int offset = 0;
+
+    public VideoFeed() {
+    	String videoDevice = "/dev/video0";
+	    int width = 640;
+	    int height = 480;
+	    int channel = 0;
+	    int videoStandard = V4L4JConstants.STANDARD_PAL;
+	    
+	    try {
+			initFrameGrabber(videoDevice, width, height, channel, videoStandard);
+		} catch (V4L4JException e) {
+			e.printStackTrace();
+		}
+	    
+        setTitle("Video Feed");
+        setVisible(true);
+        setSize(640, 480);
+
+        animation = new Thread(this);
+        animation.start();
+
+        this.addWindowListener(new WindowAdapter() {
+
+            @Override
+            public void windowClosing(
+                    WindowEvent windowEvent) {
+            	frameGrabber.stopCapture();
+                System.exit(0);
+            }
+        });
+    }
+    
+    private void initFrameGrabber(String videoDevice, int inWidth, int inHeight, int channel, int videoStandard) throws V4L4JException {
+        videoDev = new VideoDevice(videoDevice);
+    	try {
+    		List<Control> controls = videoDev.getControlList().getList();
+    		for(Control c: controls) {
+    			System.out.println(c.getName());
+    			if(c.getName().equals("Contrast"))
+    				c.setValue(CONTRAST);
+    			else if(c.getName().equals("Brightness"))
+    				c.setValue(BRIGHTNESS);
+    			else if(c.getName().equals("Hue"))
+    				c.setValue(HUE);
+    			else if(c.getName().equals("Saturation"))
+    				c.setValue(SATURATION);
+    			else if(c.getName().equals("Chroma Gain"))
+    				c.setValue(CHROMA_GAIN);
+    			else if(c.getName().equals("Chroma AGC"))
+    				c.setValue(CHROMA_AGC);
+    		}
+    	}
+    	catch(V4L4JException e3) {
+    		System.out.println("Cannot set video device settings!");
+    	}
+    	videoDev.releaseControlList();
+
+        DeviceInfo deviceInfo = videoDev.getDeviceInfo();
+
+        if (deviceInfo.getFormatList().getNativeFormats().isEmpty()) {
+        	throw new ImageFormatException("Unable to detect any native formats for the device!");
+        }
+        ImageFormat imageFormat = deviceInfo.getFormatList().getNativeFormats().get(0);
+
+        frameGrabber = videoDev.getRGBFrameGrabber(inWidth, inHeight, channel, videoStandard, imageFormat);
+
+        frameGrabber.setCaptureCallback(new CaptureCallback() {
+            public void exceptionReceived(V4L4JException e) {
+                System.err.println("Unable to capture frame:");
+                e.printStackTrace();
+            }
+
+            public void nextFrame(VideoFrame frame) {
+                image = frame.getBufferedImage();
+                
+                frame.recycle();
+            }
+        });
+
+        frameGrabber.startCapture();
+    }
+
+    public static void main(String[] args) {
+        new VideoFeed();
+    }
+
+    public void run() {
+        while (true) {
+        	try {
+				Thread.sleep(20);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+            repaint();
+        }
+    }
+
+    @Override
+    public void update(Graphics g) {
+        g.drawImage(image, 0, 0, this);
+    }
+}
