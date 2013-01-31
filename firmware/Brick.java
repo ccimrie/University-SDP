3cd6373de7ed5a80fb1d899ec83139a179a14a37
JakovS <jakovas.shmelkin@gmail.com>
2013-01-31 13:29:27 +0000

    Brick code used in the Milestone 1

--- firmware/Brick.java
+++ firmware/Brick.java
@@ -25,7 +25,7 @@ public class Brick {
 	private final static int FORWARDS = 1;
 	private final static int BACKWARDS = 2;
 	private final static int LEFT = 10;
-	private final static int RIGHT = 11;	
+	private final static int RIGHT = 11;
 	private static final int ANGLEMOVE = 12;
 
 	private final static int STOP = 3;
@@ -71,95 +71,104 @@ public class Brick {
 				LCD.drawString("opcode = " + opcode, 0, 2);
 			switch (opcode) {
 
-				case TEST:
-					boolean receiveTrue = ((opcode == 66) && (option1 == 0) && (option2 == 0) && (option3 == 66));
-					String tmp = "TEST! " + receiveTrue;
-					LCD.drawString(tmp, 0, 2);
-					byte[] testres = new byte[] { 66, 77, 88, 99 };
-					os.write(testres);
-					os.flush();
-					break;
+			case TEST:
+				boolean receiveTrue = ((opcode == 66) && (option1 == 0) && (option2 == 0) && (option3 == 66));
+				String tmp = "TEST! " + receiveTrue;
+				LCD.drawString(tmp, 0, 2);
+				byte[] testres = new byte[] { 66, 77, 88, 99 };
+				os.write(testres);
+				os.flush();
+				break;
 
-				case FORWARDS:
-					LCD.clear();
-					LCD.drawString("Forward!", 0, 2);
-					LCD.refresh();
-					//int speed1 = option1*10;
-					//int speed2 = option2*10;
-					mainMotor1(FORWARDS,5);
-					mainMotor2(FORWARDS,5);
-					Thread.sleep(10);
-					mainMotor1(FORWARDS, 700 + option1); //Left motor
-					mainMotor2(FORWARDS, 700 + option2);
-					break;
+			case FORWARDS:
+				LCD.clear();
+				LCD.drawString("Forward!", 0, 2);
+				LCD.refresh();
+				Motor.C.setAcceleration(1000);
+				Motor.B.setAcceleration(1000);
+				// int speed1 = option1*10;
+				// int speed2 = option2*10;
+				//583
+				mainMotor1(FORWARDS, 590 + option1); // Left motor
+				mainMotor2(FORWARDS, 600 + option2);
+				/*chip.sideMotor1(1, 255);
+				chip.sideMotor2(2, 255);
+				Thread.sleep(100);
+				chip.sideMotor1(2, 255);
+				chip.sideMotor2(1, 255);
+				Thread.sleep(100);
+				chip.sm1stop();
+				chip.sm2stop();*/
+				break;
 
-				case BACKWARDS:
-					LCD.clear();
-					LCD.drawString("Backward!", 0, 2);
-					LCD.refresh();
-					//while(something)
-					mainMotor1(BACKWARDS, 600);
-					mainMotor2(BACKWARDS, 600);
-					break;
+			case BACKWARDS:
+				LCD.clear();
+				LCD.drawString("Backward!", 0, 2);
+				LCD.refresh();
+				// while(something)
+				mainMotor1(BACKWARDS, 600);
+				mainMotor2(BACKWARDS, 600);
+				break;
 
-				case LEFT:
-					LCD.clear();
-					LCD.drawString("Left!", 0, 2);
-					LCD.refresh();
-					chip.sideMotor1(1, 255);
-					chip.sideMotor2(2, 255);
-					break;
+			case LEFT:
+				LCD.clear();
+				LCD.drawString("Left!", 0, 2);
+				LCD.refresh();
+				chip.sideMotor1(1, 255);
+				chip.sideMotor2(1, 255);
+				break;
 
-				case RIGHT:
-					LCD.clear();
-					LCD.drawString("Right!", 0, 2);
-					LCD.refresh();
-					chip.sideMotor1(2, 255);
-					chip.sideMotor2(1, 255);
-					break;
+			case RIGHT:
+				LCD.clear();
+				LCD.drawString("Right!", 0, 2);
+				LCD.refresh();
+				chip.sideMotor1(2, 255);
+				chip.sideMotor2(2, 255);
+				break;
 
-				case STOP:
-					stopMainMotor2();
-					stopMainMotor1();
-					chip.sm1stop();
-					chip.sm2stop();
-					Thread.sleep(10);
-					Motor.A.flt(); //Make the motors not waste battery
-					Motor.B.flt();//After they stop moving.
-					break;
+			case STOP:
+				stopMainMotor2();
+				stopMainMotor1();
+				chip.sm1stop();
+				chip.sm2stop();
+				Thread.sleep(10);
+				Motor.C.flt(); // Make the motors not waste battery
+				Motor.B.flt();// After they stop moving.
+				break;
 
-				case ROTATE:
-					LCD.clear();
-					LCD.drawString("Rotate!", 0, 2);
-					LCD.refresh(); //We get the angle by adding option1+option2 If it is greater than 0 we rotate clockwise
-					//Otherwise we rotate counterclockwise.
-					rotate(option1+option2);
-					break;
+			case ROTATE:
+				LCD.clear();
+				LCD.drawString("Rotate!", 0, 2);
+				LCD.refresh(); // We get the angle by adding option1+option2 If it is greater than 0 we rotate clockwise
+				// Otherwise we rotate counterclockwise.
+				rotate(option1 + option2);
+				break;
 
-				case KICK:
-					LCD.clear();
-					LCD.drawString("Kicking", 0, 2);
-					LCD.refresh();					
-					Motor.C.setSpeed(900);
-					Motor.C.rotateTo(60);
-					Motor.C.setSpeed(250);
-					Motor.C.rotateTo(0);
-					break;
-				case ANGLEMOVE:
-					LCD.clear();
-					LCD.drawString("Moving at an angle", 0, 2);
-					LCD.refresh();
-					byte dirMain= (byte)(((option3 & 2) > 0) ? FORWARDS : BACKWARDS);
-					byte dirSide= (byte)(((option3 & 1) > 0) ? FORWARDS : BACKWARDS);
-					mainMotor1(dirMain, 700 + option1); //Left motor
-					mainMotor2(dirMain, 700 + option1);
-					chip.sideMotor1(dirSide, option2);
-					chip.sideMotor2((dirSide == FORWARDS) ? BACKWARDS : FORWARDS, option2);
-					break;
+			case KICK:
+				LCD.clear();
+				LCD.drawString("Kicking", 0, 2);
+				LCD.refresh();
+				Motor.A.setSpeed(900);
+				Motor.A.rotateTo(70);
+				Motor.A.setSpeed(250);
+				Motor.A.rotateTo(0);
+				Motor.A.flt();
+				break;
+			case ANGLEMOVE:
+				LCD.clear();
+				LCD.drawString("Moving at an angle", 0, 2);
+				LCD.refresh();
+				byte dirMain = (byte) (((option3 & 2) > 0) ? FORWARDS : BACKWARDS);
+				byte dirSide = (byte) (((option3 & 1) > 0) ? FORWARDS : BACKWARDS);
+				mainMotor1(dirMain, 700 + option1); // Left motor
+				mainMotor2(dirMain, 700 + option1);
+				chip.sideMotor1(dirSide, option2);
+				chip.sideMotor2((dirSide == FORWARDS) ? BACKWARDS : FORWARDS, option2);
+				break;
 
-				case QUIT: // close connection
-					// Sound.twoBeeps();
-					break;
+			case QUIT: // close connection
+				// Sound.twoBeeps();
+				break;
 			}
 		}
 		// close streams and connection
@@ -175,37 +184,39 @@ public class Brick {
 
 	// Methods to activate each of the main motors individually
 	public static void mainMotor1(int direction, int speed) {
-		Motor.A.setSpeed(speed);
+		Motor.C.setAcceleration(2000);
+		Motor.C.setSpeed(speed);
 		switch (direction) {
-			case FORWARDS:
-				Motor.A.forward();
-				stateMM1 = 1;
-				break;
-			case BACKWARDS:
-				Motor.A.backward();
-				stateMM1 = 2;
-				break;
+		case FORWARDS:
+			Motor.C.forward();
+			stateMM1 = 1;
+			break;
+		case BACKWARDS:
+			Motor.C.backward();
+			stateMM1 = 2;
+			break;
 		}
 	}
 
 	public static void mainMotor2(int direction, int speed) {
+		Motor.B.setAcceleration(2000);
 		Motor.B.setSpeed(speed);
 		switch (direction) {
-			case FORWARDS:
-				Motor.B.forward();
-				stateMM2 = 1;
-				break;
-			case BACKWARDS:
-				Motor.B.backward();
-				stateMM2 = 1;
-				break;
+		case FORWARDS:
+			Motor.B.forward();
+			stateMM2 = 1;
+			break;
+		case BACKWARDS:
+			Motor.B.backward();
+			stateMM2 = 1;
+			break;
 		}
 	}
 
 	// Methods for stopping each of the main motors individually
 	public static void stopMainMotor1() throws InterruptedException {
 		if (stateMM1 != 0) {
-			Motor.A.stop(true);
+			Motor.C.stop(true);
 			stateMM1 = 0;
 		}
 	}
@@ -216,46 +227,44 @@ public class Brick {
 			stateMM2 = 0;
 		}
 	}
-	
+
 	public static void rotate(int angle) {
-		if (angle>0){ //Rotate clockwise
+		if (angle > 0) { // Rotate clockwise
 			chip.sideMotor1(1, 255);
 			chip.sideMotor2(1, 255);
-			Motor.A.setSpeed(700);
+			Motor.C.setSpeed(700);
 			Motor.B.setSpeed(700);
-			Motor.A.rotate(angle+15, true);
-			Motor.B.rotate(-angle-15);
-			chip.sideMotor1(0,0);
-			chip.sideMotor2(0,0);
-			Motor.A.flt(); //Make the motors not waste battery
+			Motor.C.rotate(angle + 15, true);
+			Motor.B.rotate(-angle - 15);
+			chip.sideMotor1(0, 0);
+			chip.sideMotor2(0, 0);
+			Motor.C.flt(); // Make the motors not waste battery
 			Motor.B.flt();
-		} else { //Rotate couterclockwise
+		} else { // Rotate couterclockwise
 			chip.sideMotor1(2, 255);
 			chip.sideMotor2(2, 255);
-			Motor.A.setSpeed(700);
+			Motor.C.setSpeed(700);
 			Motor.B.setSpeed(700);
-			Motor.A.rotate(angle-15, true);
-			Motor.B.rotate(-angle+15);
-			chip.sideMotor1(0,0);
-			chip.sideMotor2(0,0);
-			Motor.A.flt(); //Make the motors not waste battery
+			Motor.C.rotate(angle - 15, true);
+			Motor.B.rotate(-angle + 15);
+			chip.sideMotor1(0, 0);
+			chip.sideMotor2(0, 0);
+			Motor.C.flt(); // Make the motors not waste battery
 			Motor.B.flt();
 		}
 	}
-	
-	public static void maintainspeed(int option1, int option2) throws InterruptedException{
-		while (true){
-			Thread.sleep(20);
-			if (Motor.A.getTachoCount() == Motor.B.getTachoCount()){
-				mainMotor1(FORWARDS, 700 + option1); //Left motor
-				mainMotor2(FORWARDS, 700 + option2);
-			}else if(Motor.A.getTachoCount() > Motor.B.getTachoCount()){
-				mainMotor1(FORWARDS, 700 + option1); //Left motor
-				mainMotor2(FORWARDS, 700 + option2+5);
-			}else{
-				mainMotor1(FORWARDS, 700 + option1-5); //Left motor
-				mainMotor2(FORWARDS, 700 + option2);
-			}
+
+	public static void maintainSpeed(int option1, int option2) throws InterruptedException {
+		if (Motor.C.getTachoCount() == Motor.B.getTachoCount()) {
+			mainMotor1(FORWARDS, 600 + option1); // Left motor
+			mainMotor2(FORWARDS, 600 + option2);
+		} else if (Motor.C.getTachoCount() > Motor.B.getTachoCount()) {
+			mainMotor1(FORWARDS, 600 + option1 - 5); // Left motor
+			mainMotor2(FORWARDS, 600 + option2 + 5);
+		} else {
+			mainMotor1(FORWARDS, 600 + option1 + 5); // Left motor
+			mainMotor2(FORWARDS, 600 + option2 - 5);
 		}
+		Thread.sleep(100);
 	}
 }
