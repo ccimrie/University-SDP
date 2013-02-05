45f31ed2589e5ed2923e3c8ede3f90e131afd030
JakovS <jakovas.shmelkin@gmail.com>
2013-02-05 18:58:02 +0000

    Refactored the Brick and Mux codes. New rotation code, new generic movement code.

--- firmware/Mux.java
+++ firmware/Mux.java
@@ -1,90 +1,91 @@
 import lejos.nxt.I2CPort;
 import lejos.nxt.I2CSensor;
 
+/**
+ * Provides possible movement actions for the robot
+ * 
+ * @author Jakov Smelkin
+ * @author Nikolay Bogoychev
+ */
 public class Mux extends I2CSensor {
 
-	// Speeds of the side motors 0 to 255.
-	private int speed1 = 0;
-	private int speed2 = 0;
-
-	// State(Direction): 0 - float, 1 - Forward, 2 - Backwards 3 - Break.
-	private int stateSM1 = 0;
-	private int stateSM2 = 0;
-
+	private static final byte FLOAT = 0;
+	private final static int FORWARDS = 1;
+	private final static int BACKWARDS = 2;
 	// Stopping time (in miliseconds)
-	private final int stopTime = 10;
+	private static final int STOPTIME = 10;
+	private sideMotor backMotor;
+	private sideMotor frontMotor;
 
+	@SuppressWarnings("deprecation")
 	public Mux(I2CPort port) {
 		super(port);
 		// Read the morse code for the i2c board. Ours happens to be 0xB4.
 		// setAddress() function is deprecated. Perhaps investigate into the future for
 		// something better but for now it is enough.
 		setAddress(0xB4);
+		backMotor = new sideMotor(0x01, 0x02, FLOAT, 0);
+		frontMotor = new sideMotor(0x03, 0x04, FLOAT, 0);
 	}
 
-	// Controlling the state (direction) of the motor and its speed.
-	// Since Java uses singed byte value and the motors take an unsinged one,
-	// we convert a
-	public void sideMotor1(int direction, int speed) {
-		speed1 = speed;
-		sendData((byte) 0x01, (byte) direction);
-		sendData((byte) 0x02, (byte) (speed - 256));
-		stateSM1 = direction;
-	}
+	// Spins a specific motor
+	public void move(int motor, int direction, int speed) {
+		switch (motor) {
 
-	public void sideMotor2(int direction, int speed) {
-		speed2 = speed;
-		sendData((byte) 0x03, (byte) direction);
-		sendData((byte) 0x04, (byte) (speed - 256));
-		stateSM2 = direction;
-	}
+		case 1:
+			frontMotor.move(direction, speed);
+			break;
 
-	// Since breaking signal has potential of breaking the mux,
-	// what we do instead is run the motor in the opposite direction for a small time,
-	// effectively cancelling the momentum that we currently have.
-
-	public void sm1stop() throws InterruptedException {
-		switch (stateSM1) {
-			case 0:
-				// Motor is not moving, do nothing
-				break;
-			case 1:
-				// Motor is moving forward, stop it
-				sideMotor1(2, speed1);
-				Thread.sleep(stopTime);
-				sideMotor1(0, 0);
-				stateSM1 = 0;
-				break;
-			case 2:
-				// Motor is moving backwards, stop it
-				sideMotor1(1, speed1);
-				Thread.sleep(stopTime);
-				sideMotor1(0, 0);
-				stateSM1 = 0;
-				break;
+		case 2:
+			backMotor.move(direction, speed);
+			break;
 		}
 	}
 
-	public void sm2stop() throws InterruptedException {
-		switch (stateSM2) {
-			case 0:
-				// Motor is not moving, do nothing
-				break;
-			case 1:
-				// Motor is moving forward, stop it
-				sideMotor2(2, speed2);
-				Thread.sleep(stopTime);
-				sideMotor2(0, 0);
-				stateSM2 = 0;
-				break;
-			case 2:
-				// Motor is moving backwards, stop it
-				sideMotor2(1, speed2);
-				Thread.sleep(stopTime);
-				sideMotor2(0, 0);
-				stateSM2 = 0;
-				break;
-		}
+	// Stops both motors
+	public void stop() throws InterruptedException {
+		backMotor.stop();
+		frontMotor.stop();
 	}
 
+	private class sideMotor {
+		// A class to control a side motor
+		private byte directionAddr;
+		private byte speedAddr;
+		private int state; // State(Direction): 0 - float, 1 - Forward, 2 - Backwards 3 - Break.
+		private int speed; // Speed from 0 to 255.
+
+		public sideMotor(int directionAddr, int speedAddr, int state, int speed) {
+			super();
+			this.directionAddr = (byte) directionAddr;
+			this.speedAddr = (byte) speedAddr;
+			this.state = (byte) state;
+			this.speed = (byte) speed;
+		}
+
+		// Makes the motor spin in a specified direction
+		public void move(int dir, int spd) {
+			sendData(directionAddr, (byte) dir);
+			sendData(speedAddr, (byte) (spd - 256));
+			speed = spd;
+			state = dir;
+		}
+
+		// Stops the motor if not stopped already
+		public void stop() throws InterruptedException {
+			switch (state) {
+			case FLOAT: // Motor is not moving, do nothing
+				break;
+			case FORWARDS: // Motor is moving forward, stop it
+				move(BACKWARDS, speed);
+				break;
+			case BACKWARDS: // Motor is moving backwards, stop it
+				move(FORWARDS, speed);
+				break;
+			}
+			Thread.sleep(STOPTIME);
+			move(FLOAT, 0);
+		}
+
+	}
 }
