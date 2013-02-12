8f9a85af517706d094261fac94bfc5bce7526b9a
Simona Petrova <s1020153@strontian.inf.ed.ac.uk>
2013-02-12 12:49:31 +0000

    Dribble code updated

--- Group4SDP/src/strategy/planning/DribbleBall2.java
+++ Group4SDP/src/strategy/planning/DribbleBall2.java
@@ -5,6 +5,9 @@ import world.state.RobotController;
 
 import world.state.Ball;
 import world.state.Robot;
+import world.state.PitchInfo;
+
+import geometry.Vector;
 
 //author: SP
 
@@ -20,34 +23,63 @@ public class DribbleBall2 extends Strategy {
 		Robot us = worldState.ourRobot;
 		Ball ball = worldState.ball;
 
-		// Rotate so that you face the door
+		// Rotate so that the robot faces the door
+		//In order to face the door we need us.bearing == 90.
 		double angle = 90.0 - us.bearing;
 		if (angle > 180) angle = -360 + angle;
 		robot.rotate((int) angle);
 
-		// Move backwards along the x axis until aligning properly
-		robot.move(0, -127);
-		while ((ball.x - threshold) < us.x) {
-			Thread.sleep(100);
+		//Check if the robot is in front of the ball on the y axis and go around the ball
+		if (((us.y < ball.y + threshold) || (us.y > ball.y - threshold)) && (us.x>ball.x)) {
+			//Check which is the safer side to go around the ball
+			if (Math.abs(us.y  - PitchInfo.safeUpperBoundSide.getY()) > Math.abs(us.y  - PitchInfo.safeLowerBoundSide.getY())){
+				while ((ball.y - threshold) < us.y){
+					robot.move(-127, 0); //Robot's left
+				}
+				robot.stop();
+			}
+			else {
+				while ((ball.y + threshold) < us.y){
+					robot.move(127, 0); //Robot's right
+				}
+				robot.stop();
+			}
 		}
-		robot.stop();
-
-		// Move either left or right along the y axis until aligning properly
+		
+		//The condition checks where the robot is in respect to the ball and aligns it properly on the x axis
+		//If the robot is ahead of the ball on the pitch
+		if (us.x > ball.x){
+			while ((ball.x - threshold) < us.x) {	
+				robot.move(0, -127);
+				Thread.sleep(100);
+			}
+			robot.stop();
+		}
+		//If the robot is already behind the ball on the pitch
+		else {
+			while ((ball.x - threshold) > us.x) {
+				robot.move(0, -127);
+				Thread.sleep(100);
+			}
+			robot.stop();
+		}
+	
+		// Move either up or down along the y axis until aligning properly
 		if (us.y > ball.y) {
-			System.out.println("Moving down the y axis");
-			robot.move(-127, 0);
+			System.out.println("Moving up the y axis"); //Robot's left
+			
 			while (us.y > ball.y) {
+				robot.move(-127, 0);
 				Thread.sleep(200);
 			}
-
 		} else {
-			System.out.println("Moving up the y axis");
+			System.out.println("Moving down the y axis"); //Robot's right
 			robot.move(127, 0);
 			while (us.y < ball.y) {
 				Thread.sleep(200);
 			}
 		}
-		// Now dribble I guess
+		// Now dribble 
 		robot.move(0, 10);
 	}
 
