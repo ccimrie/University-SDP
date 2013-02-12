2c76a7e098560846aba61dccfcad8428ef51377e
XapaJIaMnu <nheart@gmail.com>
2013-02-12 18:48:05 +0000

    Example strategy module, look at this when implementing your modules

--- Group4SDP/src/strategy/planning/Offensive.java
+++ Group4SDP/src/strategy/planning/Offensive.java
@@ -6,39 +6,45 @@ import strategy.calculations.IsRobotFacingPoint;
 import strategy.movement.AvoidanceStrategy;
 import strategy.movement.GoToPoint;
 import world.state.Robot;
-import world.state.World;
+import world.state.RobotController;
+import vision.WorldState;
 
-import comms.control.Server;
 
-public class Offensive {
-	
+public class Offensive extends StrategyInterface implements Runnable{
 
-	public void offensive(World world, Server server){
-				
-		Robot us = world.getOurRobot();
-		Vector theirGoal = world.getTheirGoal();
 
-		if((us.x < world.getMidPoint() && world.ourHalfLeft()) 
-		|| (us.x >= world.getMidPoint() && !world.ourHalfLeft())){
-			
-			System.out.println("OUR HALF YEAH!");
-			GoToPoint.goToPoint(world, server, theirGoal, AvoidanceStrategy.Aggressive);
-						
-		} else {
-			
-			double angle = IsRobotFacingPoint.Turner(us, theirGoal.getX(), theirGoal.getY());
-			System.out.println(Math.abs(angle));
-			if(Math.abs(angle) > 40){
-				
-				System.out.println("Their half wrong angle");
-				GoToPoint.goToPoint(world, server, theirGoal, AvoidanceStrategy.Aggressive);
-				
+	public Offensive(WorldState world, Robot us, Robot them, RobotController rc){
+		super(world, us, them, rc);
+	}
+
+	public void run(){
+		while (!shouldidie && !Strategy.alldie){		
+			Vector theirGoal = world.getTheirGoal();
+
+			if((us.x < world.getMidPoint() && world.ourHalfLeft()) 
+					|| (us.x >= world.getMidPoint() && !world.ourHalfLeft())){
+
+				System.out.println("OUR HALF YEAH!");
+				//GoToPoint.goToPoint(world, server, theirGoal, AvoidanceStrategy.Aggressive);
+
 			} else {
-				
-				System.out.println("Kicking!");
-				server.kick();
+
+				double angle = IsRobotFacingPoint.Turner(us, theirGoal.getX(), theirGoal.getY());
+				System.out.println(Math.abs(angle));
+				if(Math.abs(angle) > 40){
+
+					System.out.println("Their half wrong angle");
+					//Go to point not implemented.
+					//	GoToPoint.goToPoint(world, server, theirGoal, AvoidanceStrategy.Aggressive);
+
+				} else {
+
+					System.out.println("Kicking!");
+					rc.kick();
+				}
+
 			}
-		
 		}
+		rc.stop();
 	}
 }
