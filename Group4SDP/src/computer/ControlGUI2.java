7377f00197acb804aab35aff87930676c8833ca6
JakovS <jakovas.shmelkin@gmail.com>
2013-02-26 20:21:06 +0000

    Movement code, how-to on usage

--- Group4SDP/src/computer/ControlGUI2.java
+++ Group4SDP/src/computer/ControlGUI2.java
@@ -232,13 +232,13 @@ public class ControlGUI2 extends JFrame {
 		start.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				// Run in a new thread to free up UI while running
-				Movement m = new Movement(worldState, robot);
-				try {
-					m.moveToPoint(538, 191);
-				} catch (InterruptedException e1) {
+				//Movement m = new Movement(worldState, robot);
+				//try {
+					//m.moveToPoint(538, 191);
+				//} catch (InterruptedException e1) {
 					// TODO Auto-generated catch block
-					e1.printStackTrace();
-				}
+				//	e1.printStackTrace();
+				//}
 				/*for (int i=90; i<=7200; i+=360){
 					m.move(Math.toRadians(i));
 					try {
@@ -271,12 +271,8 @@ public class ControlGUI2 extends JFrame {
 		stratStart.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				// Run in a new thread to free up UI while running
-				//Thread strat = new Thread (new Strategy(worldState, robot));
-				//strat.start();
-				System.out.println(" Starting the domination");
-				Offensive anihilation = new Offensive(worldState, worldState.getOurRobot(), 
-						worldState.getTheirRobot(), robot);
-				anihilation.run();
+				Thread strat = new Thread (new Strategy(worldState, robot));
+				strat.start();
 			}
 		});
 		
