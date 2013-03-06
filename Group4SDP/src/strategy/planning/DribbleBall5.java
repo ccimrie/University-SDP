5940e672616266c49171df4b327c1596048998ef
Alex Adams <alex-j-a@hotmail.co.uk>
2013-03-06 09:15:40 +0000

    Tweaks to DribbleBall

--- Group4SDP/src/strategy/planning/DribbleBall5.java
+++ Group4SDP/src/strategy/planning/DribbleBall5.java
@@ -12,11 +12,10 @@ import world.state.Ball;
 import world.state.Robot;
 import world.state.WorldState;
 
-
 public class DribbleBall5 {
 
 	private final ScheduledExecutorService sleepScheduler = Executors
-	.newScheduledThreadPool(1);
+			.newScheduledThreadPool(1);
 
 	/** Thread-safe sleep */
 	private void safeSleep(long millis) throws InterruptedException {
@@ -39,7 +38,7 @@ public class DribbleBall5 {
 	private static double dribbleDistance = 200;
 
 	public void dribbleBall(WorldState worldState, RobotMover mover)
-	throws InterruptedException {
+			throws InterruptedException {
 		// Get robot and ball from world
 		worldState.setOurRobot();
 		Robot us = worldState.ourRobot;
@@ -50,33 +49,34 @@ public class DribbleBall5 {
 		int goalx = targetgoal.getX();
 		int goaly = targetgoal.getY();
 
-		//Determine a different position behind the ball depending on which door we are shooting in
+		// Determine a different position behind the ball depending on which
+		// door we are shooting in
 		if (die)
 			return;
-		if (worldState.areWeOnLeft()){		
-			double slope = (238 - ball.y)/(606 - ball.x);
-			mover.moveToAStar(ball.x - 70, ball.y + slope*70 , true, false);
+		if (worldState.areWeOnLeft()) {
+			double slope = (238 - ball.y) / (606 - ball.x);
+			mover.moveToAStar(ball.x - 70, ball.y + slope * 70, true, false);
 			mover.delay(50);
-			mover.moveToAndStop(ball.x - 70, ball.y + slope*70);
+			mover.moveToAndStop(ball.x - 70, ball.y + slope * 70);
 			mover.waitForCompletion();
 
 		} else {
-			double slope = (33 - ball.y)/(247 - ball.x);
-			mover.moveToAStar(ball.x + 70, ball.y + 70*slope, true, false);
+			double slope = (247 - ball.y) / (33 - ball.x);
+			mover.moveToAStar(ball.x + 70, ball.y + 70 * slope, true, false);
 			mover.delay(50);
-			mover.moveToAndStop(ball.x - 70, ball.y + slope*70);
+			mover.moveToAndStop(ball.x + 70, ball.y + slope * 70);
 			mover.waitForCompletion();
 		}
 		double angle = TurnToBall.AngleTurner(us, ball.x, ball.y);
 		safeSleep(50);
 		int attempt = 0;
-		System.out.printf("Angle is, %d\n", (int)angle);
+		System.out.printf("Angle is, %d\n", (int) angle);
 		while (Math.abs(angle) > 15 && attempt < 10) {
 			if ((Math.abs(angle) > 10) && (Math.abs(angle) < 50)) {
 				mover.rotate(Math.toRadians(angle / 3));
 				mover.waitForCompletion();
 			} else if (Math.abs(angle) > 50) {
-				mover.rotate(Math.toRadians(angle/2));
+				mover.rotate(Math.toRadians(angle / 2));
 				mover.waitForCompletion();
 			}
 			++attempt;
@@ -91,14 +91,12 @@ public class DribbleBall5 {
 
 		mover.move(0, 50);
 
-		mover.delay(800);
-		//double previousy = ball.y;
-		/*while(true){
-			if (((Math.abs(previousx - ball.x)) > 10) ||
-					((Math.abs(previousy) - ball.y) >10)){
-				break;
-			}
-		}*/
+		mover.delay(1200);
+		// double previousy = ball.y;
+		/*
+		 * while(true){ if (((Math.abs(previousx - ball.x)) > 10) ||
+		 * ((Math.abs(previousy) - ball.y) >10)){ break; } }
+		 */
 
 		mover.kick();
 		mover.delay(20);
