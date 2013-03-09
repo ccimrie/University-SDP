6dd1849467f037379e81abc1ac0ca78eb0c3c082
Bachir Djelmami-Hani <s1142191@knapdale.inf.ed.ac.uk>
2013-03-09 18:28:21 +0000

    The update worldstate basically works, the only problem remains the orientation (will fix that on sunday/monday)

--- Group4SDP/src/simulator/TestSim.java
+++ Group4SDP/src/simulator/TestSim.java
@@ -13,6 +13,9 @@ import org.jbox2d.testbed.framework.j2d.TestPanelJ2D;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import strategy.calculations.GoalInfo;
+
+import vision.PitchConstants;
 import world.state.Ball;
 import world.state.WorldState;
 
@@ -25,34 +28,64 @@ public class TestSim {
 
 
 	
-
+		 static PitchConstants pitchConstants = new PitchConstants(0);
+		static GoalInfo goalInfo = new GoalInfo(pitchConstants);
+		 static WorldState worldState = new WorldState(goalInfo);
 	    public static void main(String[] args) {
-	        try {
-	            UIManager.setLookAndFeel("com.sun.java.swing.plaf.nimbus.NimbusLookAndFeel");
-	        } catch (Exception e) {
-	           
-	        }
-	        //Instantiate model where all the tests reside.
-	        TestbedModel model = new TestbedModel();
-	        TestbedPanel panel = new TestPanelJ2D(model);
 
-	        //Instantiate new custom test.
-	        Updateworld matchTest = new Updateworld();
+	        Thread thread1 = new Thread () {
+	        	public void run () {
+	        		try {
+	    	            UIManager.setLookAndFeel("com.sun.java.swing.plaf.nimbus.NimbusLookAndFeel");
+	    	        } catch (Exception e) {
+	    	           
+	    	        }
+	    	        //Instantiate model where all the tests reside.
+	    	        TestbedModel model = new TestbedModel();
+	    	        TestbedPanel panel = new TestPanelJ2D(model);
+
+	    	        //Instantiate new custom test.
+	    	        Updateworld matchTest = new Updateworld(worldState);
+	    	        
+	    	       
+	    	        
+	    	        //Add test to the model
+	    	        model.addCategory("Robots");
+	    	        model.addTest(matchTest);
+
+	    	        //Add the default tests which were included in JBox2D distro.
+	    	        TestList.populateModel(model);
+	    	        JFrame testbed = new TestbedFrame(model, panel);
+	    	        testbed.setVisible(true);
+	    	        testbed.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+	               
+	        		}
+	        			};
+
 	        
 	       
+	      
+	        Thread thread2 = new Thread() {
+	        	public void run(){
+	        		while(true){
+	        			
+	        			//System.out.println("Ball x :" + worldState.getBallX() + "Ball y :" + worldState.getBallY());
+	        			//System.out.println("Blue x :" + worldState.getBlueX() + " Blue y :" + worldState.getBlueY());
+	        			//System.out.println("Yellow x :" + worldState.getYellowX() + " Yellow y :" + worldState.getYellowX());
+	        			System.out.println("Blue Orientation :" + worldState.getBlueOrientation() + "Yellow Orientation :" + worldState.getYellowOrientation());
+	        			
+	        		}
+	        		
+	        		
+	        	}
+	        	
+	        	
+	        };
+
+	        thread1.start();
+	        thread2.start();
 	        
-	        //Add test to the model
-	        model.addCategory("Robots");
-	        model.addTest(matchTest);
-
-	        //Add the default tests which were included in JBox2D distro.
-	        TestList.populateModel(model);
-	        JFrame testbed = new TestbedFrame(model, panel);
-	        testbed.setVisible(true);
-	        testbed.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-
-	
 	    }
 
 }
