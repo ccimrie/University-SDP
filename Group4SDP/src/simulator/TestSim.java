58f70b5868cf2a0fd75b41700710c1aab8d88a61
Bachir Djelmami-Hani <s1142191@inishail.inf.ed.ac.uk>
2013-03-10 16:37:07 +0000

    modifications to the UpdateWorld

--- Group4SDP/src/simulator/TestSim.java
+++ Group4SDP/src/simulator/TestSim.java
@@ -3,90 +3,89 @@ package simulator;
 import javax.swing.JFrame;
 import javax.swing.UIManager;
 
-import org.jbox2d.dynamics.Body;
 import org.jbox2d.testbed.framework.TestList;
 import org.jbox2d.testbed.framework.TestbedFrame;
-import org.jbox2d.testbed.framework.TestbedMain;
 import org.jbox2d.testbed.framework.TestbedModel;
 import org.jbox2d.testbed.framework.TestbedPanel;
 import org.jbox2d.testbed.framework.j2d.TestPanelJ2D;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import strategy.calculations.GoalInfo;
-
+import utility.SafeSleep;
 import vision.PitchConstants;
-import world.state.Ball;
 import world.state.WorldState;
 
 public class TestSim {
-	 
-	
-	
 
-	    public static float x, y, ballx, bally, oppx, oppy, bearing, oppBearing;
+	public static float x, y, ballx, bally, oppx, oppy, bearing, oppBearing;
 
+	public static void main(String[] args) {
+		final PitchConstants pitchConstants = new PitchConstants(0);
+		final GoalInfo goalInfo = new GoalInfo(pitchConstants);
+		final WorldState worldState = new WorldState(goalInfo);
 
-	
-		 static PitchConstants pitchConstants = new PitchConstants(0);
-		static GoalInfo goalInfo = new GoalInfo(pitchConstants);
-		 static WorldState worldState = new WorldState(goalInfo);
-	    public static void main(String[] args) {
+		Thread thread1 = new Thread() {
+			public void run() {
+				try {
+					UIManager
+							.setLookAndFeel("com.sun.java.swing.plaf.nimbus.NimbusLookAndFeel");
+				} catch (Exception e) {
 
-	        Thread thread1 = new Thread () {
-	        	public void run () {
-	        		try {
-	    	            UIManager.setLookAndFeel("com.sun.java.swing.plaf.nimbus.NimbusLookAndFeel");
-	    	        } catch (Exception e) {
-	    	           
-	    	        }
-	    	        //Instantiate model where all the tests reside.
-	    	        TestbedModel model = new TestbedModel();
-	    	        TestbedPanel panel = new TestPanelJ2D(model);
+				}
+				// Instantiate model where all the tests reside.
+				TestbedModel model = new TestbedModel();
+				TestbedPanel panel = new TestPanelJ2D(model);
 
-	    	        //Instantiate new custom test.
-	    	        Updateworld matchTest = new Updateworld(worldState);
-	    	        
-	    	       
-	    	        
-	    	        //Add test to the model
-	    	        model.addCategory("Robots");
-	    	        model.addTest(matchTest);
+				// Instantiate new custom test.
+				Updateworld matchTest = new Updateworld(worldState);
 
-	    	        //Add the default tests which were included in JBox2D distro.
-	    	        TestList.populateModel(model);
-	    	        JFrame testbed = new TestbedFrame(model, panel);
-	    	        testbed.setVisible(true);
-	    	        testbed.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+				// Add test to the model
+				model.addCategory("Robots");
+				model.addTest(matchTest);
 
-	               
-	        		}
-	        			};
+				// Add the default tests which were included in JBox2D distro.
+				TestList.populateModel(model);
+				JFrame testbed = new TestbedFrame(model, panel);
+				testbed.setVisible(true);
+				testbed.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-	        
-	       
-	      
-	        Thread thread2 = new Thread() {
-	        	public void run(){
-	        		while(true){
-	        			
-	        			//System.out.println("Ball x :" + worldState.getBallX() + "Ball y :" + worldState.getBallY());
-	        			//System.out.println("Blue x :" + worldState.getBlueX() + " Blue y :" + worldState.getBlueY());
-	        			//System.out.println("Yellow x :" + worldState.getYellowX() + " Yellow y :" + worldState.getYellowX());
-	        			System.out.println("Blue Orientation :" + worldState.getBlueOrientation());
-	        			System.out.println("Yellow Orientation :" + worldState.getYellowOrientation());
-	        			
-	        		}
-	        		
-	        		
-	        	}
-	        	
-	        	
-	        };
+			}
+		};
 
-	        thread1.start();
-	        thread2.start();
-	        
-	    }
+		Thread thread2 = new Thread() {
+			public void run() {
+				while (true) {
+					try {
+						SafeSleep.sleep(100);
+					} catch (InterruptedException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+
+					//System.out.println("Ball x :" + worldState.getBallX()
+					//		+ " y :" + worldState.getBallY());
+				//	System.out.println("Blue x :" + worldState.getBlueX()
+				//			+ " y :" + worldState.getBlueY());
+				//	System.out.println("Yellow x :" + worldState.getYellowX()
+				//			+ " y :" + worldState.getYellowY());
+					// System.out.println("Blue Orientation :" +
+					// worldState.getBlueOrientation());
+					 System.out.println("Yellow Orientation :" +
+					 Math.toDegrees(worldState.getYellowOrientation()));
+
+				}
+
+			}
+
+		};
+
+		thread1.start();
+		thread2.start();
+		try {
+			thread1.join();
+			thread2.join();
+		} catch (InterruptedException e) {
+
+		}
+	}
 
 }
--- Group4SDP/src/simulator/Updateworld.java
+++ Group4SDP/src/simulator/Updateworld.java
@@ -9,7 +9,7 @@ import org.jbox2d.dynamics.BodyType;
 import org.jbox2d.dynamics.FixtureDef;
 import org.jbox2d.testbed.framework.TestbedSettings;
 import org.jbox2d.testbed.framework.TestbedTest;
-import world.state.*; 
+import world.state.*;
 import vision.*;
 
 import org.jbox2d.collision.shapes.PolygonShape;
@@ -23,6 +23,7 @@ import org.jbox2d.dynamics.World;
 import org.jbox2d.dynamics.joints.PrismaticJoint;
 import org.jbox2d.dynamics.joints.PrismaticJointDef;
 import strategy.calculations.*;
+
 /**
  * 
  * This class will serve the purpose of updating the worldState
@@ -30,218 +31,225 @@ import strategy.calculations.*;
  * work in progress...
  * 
  * Bach
+ * 
  * @author s1142191
- *
+ * 
  */
 public class Updateworld extends TestbedTest {
-    public static final float pitchL = 2.4384f;
-    public static final float pitchW = 1.2192f;
-    public static final float goalW = 0.6f;
+	public static final float pitchL = 2.4384f;
+	public static final float pitchW = 1.2192f;
+	public static final float goalW = 0.6f;
 
-    public static float scale = 20.0f;
+	public static float scale = 20.0f;
 
-    boolean firstTime = true;
-    boolean simulatorMode = false;
-    public SimDisplayRobot ourRobotSim, theirRobotSim;
-    boolean argDeserialized = true;
-    
-    public Body ballSim;
+	boolean firstTime = true;
+	boolean simulatorMode = false;
+	public SimDisplayRobot ourRobotSim, theirRobotSim;
+	boolean argDeserialized = true;
 
-    WorldState worldState;
-   
-    
-    
-    
+	public Body ballSim;
+
+	private final WorldState worldState;
 
 	Robot ourRobot = new Robot(RobotType.Us);
 	Robot theirRobot = new Robot(RobotType.Them);
-	
-	Ball ball = new Ball();
-	
- 
 
-    Body pitch;   
+	Ball ball = new Ball();
+
+	Body pitch;
+
 	public Updateworld(WorldState worldState) {
 		// Set the state fields.
 		this.worldState = worldState;
-	
+
 	}
 
+	public boolean isSaveLoadEnabled() {
+		return true;
+	}
 
-    public boolean isSaveLoadEnabled() {
-        return true;
-    }
+	public void initialisePitch() {
+		BodyDef bd = new BodyDef();
+		this.pitch = getWorld().createBody(bd);
+		getWorld().setGravity(new Vec2(0.0f, 0.0f));
 
+		PolygonShape shape = new PolygonShape();
+		// Bottom wall
+		shape.setAsEdge(new Vec2(0.0f * scale, 0.0f * scale), new Vec2(pitchL
+				* scale, 0.0f * scale));
+		this.pitch.createFixture(shape, 0.0f);
+		// Top wall
+		shape.setAsEdge(new Vec2(0.0f * scale, pitchW * scale), new Vec2(pitchL
+				* scale, pitchW * scale));
+		this.pitch.createFixture(shape, 0.0f);
+		// Left wall
+		shape.setAsEdge(new Vec2(0.0f * scale, 0.0f * scale), new Vec2(
+				0.0f * scale, pitchW * scale / 2 - goalW * scale / 2));
+		this.pitch.createFixture(shape, 0.0f);
+		shape.setAsEdge(new Vec2(0.0f * scale, pitchW * scale / 2 + goalW
+				* scale / 2), new Vec2(0.0f * scale, pitchW * scale));
+		this.pitch.createFixture(shape, 0.0f);
+		// Left goal
+		shape.setAsEdge(new Vec2(0.0f * scale, pitchW * scale / 2 + goalW
+				* scale / 2), new Vec2(-0.06f * scale, pitchW * scale / 2
+				+ goalW * scale / 2));
+		this.pitch.createFixture(shape, 0.0f);
+		shape.setAsEdge(new Vec2(0.0f * scale, pitchW * scale / 2 - goalW
+				* scale / 2), new Vec2(-0.06f * scale, pitchW * scale / 2
+				- goalW * scale / 2));
+		this.pitch.createFixture(shape, 0.0f);
+		shape.setAsEdge(new Vec2(-0.06f * scale, pitchW * scale / 2 + goalW
+				* scale / 2), new Vec2(-0.06f * scale, pitchW * scale / 2
+				- goalW * scale / 2));
+		this.pitch.createFixture(shape, 0.0f);
+		// Right wall
+		shape.setAsEdge(new Vec2(pitchL * scale, 0.0f * scale), new Vec2(pitchL
+				* scale, pitchW * scale / 2 - goalW * scale / 2));
+		this.pitch.createFixture(shape, 0.0f);
+		shape.setAsEdge(new Vec2(pitchL * scale, pitchW * scale / 2 + goalW
+				* scale / 2), new Vec2(pitchL * scale, pitchW * scale));
+		this.pitch.createFixture(shape, 0.0f);
+		// Right goal
+		shape.setAsEdge(new Vec2(pitchL * scale, pitchW * scale / 2 + goalW
+				* scale / 2), new Vec2(pitchL * scale + 0.06f * scale, pitchW
+				* scale / 2 + goalW * scale / 2));
+		this.pitch.createFixture(shape, 0.0f);
+		shape.setAsEdge(new Vec2(pitchL * scale, pitchW * scale / 2 - goalW
+				* scale / 2), new Vec2(pitchL * scale + 0.06f * scale, pitchW
+				* scale / 2 - goalW * scale / 2));
+		this.pitch.createFixture(shape, 0.0f);
+		shape.setAsEdge(new Vec2(pitchL * scale + 0.06f * scale, pitchW * scale
+				/ 2 + goalW * scale / 2), new Vec2(pitchL * scale + 0.06f
+				* scale, pitchW * scale / 2 - goalW * scale / 2));
+		this.pitch.createFixture(shape, 0.0f);
+	}
 
-    public void initialisePitch() {
-        BodyDef bd = new BodyDef();
-        this.pitch = getWorld().createBody(bd);
-        getWorld().setGravity(new Vec2(0.0f, 0.0f));
+	public WorldState getTheRealWorld() {
 
-        PolygonShape shape = new PolygonShape();
-        //Bottom wall
-        shape.setAsEdge(new Vec2(0.0f * scale, 0.0f * scale), new Vec2(pitchL * scale, 0.0f * scale));
-        this.pitch.createFixture(shape, 0.0f);
-        //Top wall
-        shape.setAsEdge(new Vec2(0.0f * scale, pitchW * scale), new Vec2(pitchL * scale, pitchW * scale));
-        this.pitch.createFixture(shape, 0.0f);
-        //Left wall
-        shape.setAsEdge(new Vec2(0.0f * scale, 0.0f * scale), new Vec2(0.0f * scale, pitchW * scale / 2 - goalW * scale / 2));
-        this.pitch.createFixture(shape, 0.0f);
-        shape.setAsEdge(new Vec2(0.0f * scale, pitchW * scale / 2 + goalW * scale / 2), new Vec2(0.0f * scale, pitchW * scale));
-        this.pitch.createFixture(shape, 0.0f);
-        //Left goal
-        shape.setAsEdge(new Vec2(0.0f * scale, pitchW * scale / 2 + goalW * scale / 2),
-                new Vec2(-0.06f * scale, pitchW * scale / 2 + goalW * scale / 2));
-        this.pitch.createFixture(shape, 0.0f);
-        shape.setAsEdge(new Vec2(0.0f * scale, pitchW * scale / 2 - goalW * scale / 2),
-                new Vec2(-0.06f * scale, pitchW * scale / 2 - goalW * scale / 2));
-        this.pitch.createFixture(shape, 0.0f);
-        shape.setAsEdge(new Vec2(-0.06f * scale, pitchW * scale / 2 + goalW * scale / 2),
-                new Vec2(-0.06f * scale, pitchW * scale / 2 - goalW * scale / 2));
-        this.pitch.createFixture(shape, 0.0f);
-        //Right wall
-        shape.setAsEdge(new Vec2(pitchL * scale, 0.0f * scale), new Vec2(pitchL * scale, pitchW * scale / 2 - goalW * scale / 2));
-        this.pitch.createFixture(shape, 0.0f);
-        shape.setAsEdge(new Vec2(pitchL * scale, pitchW * scale / 2 + goalW * scale / 2), new Vec2(pitchL * scale, pitchW * scale));
-        this.pitch.createFixture(shape, 0.0f);
-        //Right goal
-        shape.setAsEdge(new Vec2(pitchL * scale, pitchW * scale / 2 + goalW * scale / 2),
-                new Vec2(pitchL * scale + 0.06f * scale, pitchW * scale / 2 + goalW * scale / 2));
-        this.pitch.createFixture(shape, 0.0f);
-        shape.setAsEdge(new Vec2(pitchL * scale, pitchW * scale / 2 - goalW * scale / 2),
-                new Vec2(pitchL * scale + 0.06f * scale, pitchW * scale / 2 - goalW * scale / 2));
-        this.pitch.createFixture(shape, 0.0f);
-        shape.setAsEdge(new Vec2(pitchL * scale + 0.06f * scale, pitchW * scale / 2 + goalW * scale / 2),
-                new Vec2(pitchL * scale + 0.06f * scale, pitchW * scale / 2 - goalW * scale / 2));
-        this.pitch.createFixture(shape, 0.0f);
-    }
-    public WorldState getTheRealWorld(){
-    	
-    	return worldState;
-    }
-    public void createBall() {
-        CircleShape ballshape = new CircleShape();
-        ballshape.m_radius = 0.025f * scale;
+		return worldState;
+	}
 
-        FixtureDef fdb = new FixtureDef();
-        fdb.shape = ballshape;
-        fdb.density = 1.0f;
-        fdb.friction = 0.01f;
+	public void createBall() {
+		CircleShape ballshape = new CircleShape();
+		ballshape.m_radius = 0.025f * scale;
 
-        BodyDef ballbd = new BodyDef();
-        ballbd.type = BodyType.DYNAMIC;
-        ballbd.angularDamping = 1.0f;
-        ballbd.linearDamping = 0.5f;
-        ballbd.bullet = true;
-        ballbd.position.set(pitchL * scale / 2, pitchW * scale / 2);
-        this.ballSim = getWorld().createBody(ballbd);
-        this.ballSim.createFixture(fdb);
+		FixtureDef fdb = new FixtureDef();
+		fdb.shape = ballshape;
+		fdb.density = 1.0f;
+		fdb.friction = 0.01f;
 
-    }
+		BodyDef ballbd = new BodyDef();
+		ballbd.type = BodyType.DYNAMIC;
+		ballbd.angularDamping = 1.0f;
+		ballbd.linearDamping = 0.5f;
+		ballbd.bullet = true;
+		ballbd.position.set(pitchL * scale / 2, pitchW * scale / 2);
+		this.ballSim = getWorld().createBody(ballbd);
+		this.ballSim.createFixture(fdb);
 
- 
+	}
 
-    public synchronized void step(TestbedSettings settings) {
-        super.step(settings);
-        
-        
-        
-        float timestep = 1.0f/getModel().getCalculatedFps();
-       
-        
-       // ourownRobot, 
-        worldState.setBallX( (int)(Math.round((ballSim.getWorldCenter().x/scale))));
-        worldState.setBallY((int)Math.round(ballSim.getWorldCenter().y/scale));
-        
-        worldState.setBlueX((int)(Math.round((ourRobotSim.robot.getWorldCenter().x/scale))));
-        worldState.setBallY((int)Math.round(ourRobotSim.robot.getWorldCenter().y/scale));
-        
-        worldState.setYellowX((int)(theirRobotSim.robot.getWorldCenter().x/scale));
-        worldState.setYellowY((int)(theirRobotSim.robot.getWorldCenter().y/scale));
-        
-       worldState.setBlueOrientation(Math.toDegrees(Math.abs((ourRobotSim.robot.getAngle()-Math.PI/2.0f)%(2*Math.PI))) );
-        
-       
-        worldState.setYellowOrientation(Math.toDegrees(Math.abs((theirRobotSim.robot.getAngle()-Math.PI/2.0f)%(2*Math.PI))) );
-        
-       
-        
-        
-       /** 
+	private double convertAngle(double jboxAngle) {
+		double x = Math.cos(jboxAngle);
+		double y = Math.sin(jboxAngle);
 
+		double angle = Math.acos(y);
+		if (angle < 0) {
+			angle += 2.0 * Math.PI;
+		}
+		if (x < 0) {
+			angle = 2.0 * Math.PI - angle;
+		}
 
-        Thread thread1 = new Thread () {
-        	public void run () {
-                worldstate.setBallX( (int)(Math.round((ballSim.getWorldCenter().x/scale))));
-                worldstate.setBallY((int)Math.round(ballSim.getWorldCenter().y/scale));
-        		}
-        			};
+		return angle;
+	}
 
-       
-       thread1.start();
-       
-       */
-         /**
-          * 
-          *The part that need some tweaking.
-          * 
-          *Bach 
-          */
-    /**    
-        ourRobot.x =  ourRobotSim.robot.getWorldCenter().x/scale;
-		ourRobot.y = ourRobotSim.robot.getWorldCenter().y/scale;
-		ourRobot.bearing = ourRobotSim.robot.getAngle() + Math.PI/2.0f;
+	public void step(TestbedSettings settings) {
+		super.step(settings);
+
+		float timestep = 1.0f / getModel().getCalculatedFps();
+
+		Vec2 ballPos = ballSim.getPosition();
+		// ourownRobot,
+		worldState.setBallX((int) (ballPos.x * scale));
+		worldState.setBallY((int) (ballPos.y * scale));
+
+		worldState.setBlueX((int) (Math.round((ourRobotSim.robot
+				.getWorldCenter().x * scale))));
+		worldState
+				.setBlueY((int) Math.round(ourRobotSim.robot.getWorldCenter().y
+						* scale));
+
+		worldState
+				.setYellowX((int) (theirRobotSim.robot.getWorldCenter().x * scale));
+		worldState
+				.setYellowY((int) (theirRobotSim.robot.getWorldCenter().y * scale));
+
+		worldState
+				.setBlueOrientation(convertAngle(ourRobotSim.robot.getAngle()));
+
+		worldState.setYellowOrientation(convertAngle(theirRobotSim.robot
+				.getAngle()));
 		
-		theirRobot.x = theirRobotSim.robot.getWorldCenter().x/scale;
-		theirRobot.y = theirRobotSim.robot.getWorldCenter().y/scale;
-		theirRobot.bearing = theirRobotSim.robot.getAngle() + Math.PI/2.0f;
-		
-	ball.x = ballSim.getWorldCenter().x/scale;
-	ball.y = ballSim.getWorldCenter().y/scale;
-        */
-        
-       
+		worldState.update();
+		/**
+		 * Thread thread1 = new Thread () { public void run () {
+		 * worldstate.setBallX(
+		 * (int)(Math.round((ballSim.getWorldCenter().x/scale))));
+		 * worldstate.setBallY
+		 * ((int)Math.round(ballSim.getWorldCenter().y/scale)); } };
+		 * 
+		 * 
+		 * thread1.start();
+		 */
+		/**
+		 * 
+		 * The part that need some tweaking.
+		 * 
+		 * Bach
+		 */
+		/**
+		 * ourRobot.x = ourRobotSim.robot.getWorldCenter().x/scale; ourRobot.y =
+		 * ourRobotSim.robot.getWorldCenter().y/scale; ourRobot.bearing =
+		 * ourRobotSim.robot.getAngle() + Math.PI/2.0f;
+		 * 
+		 * theirRobot.x = theirRobotSim.robot.getWorldCenter().x/scale;
+		 * theirRobot.y = theirRobotSim.robot.getWorldCenter().y/scale;
+		 * theirRobot.bearing = theirRobotSim.robot.getAngle() + Math.PI/2.0f;
+		 * 
+		 * ball.x = ballSim.getWorldCenter().x/scale; ball.y =
+		 * ballSim.getWorldCenter().y/scale;
+		 */
 
-  
-        
-    }
-
+	}
 
 	@Override
 	public String getTestName() {
 		// TODO Auto-generated method stub
-		 return "Synergy Team simulator";
+		return "Synergy Team simulator";
 	}
-
 
 	@Override
 	public void initTest(boolean argDeserialized) {
-        if (this.firstTime) {
-            setCamera(new Vec2(pitchL * scale / 2, pitchW * scale / 2), 0.5f * scale);
-            this.firstTime = false;
-        }
-        if (argDeserialized) {
-            return;
-        }
-        {
-            initialisePitch();
-            createBall();
-           
-            this.ourRobotSim = new SimDisplayRobot(true, getWorld());
-            this.theirRobotSim = new SimDisplayRobot(false, getWorld());
-            
-          //  this.world = new SimWorld();
-        }
-		
+		if (this.firstTime) {
+			setCamera(new Vec2(pitchL * scale / 2, pitchW * scale / 2),
+					0.5f * scale);
+			this.firstTime = false;
+		}
+		if (argDeserialized) {
+			return;
+		}
+		{
+			initialisePitch();
+			createBall();
+
+			this.ourRobotSim = new SimDisplayRobot(true, getWorld());
+			this.theirRobotSim = new SimDisplayRobot(false, getWorld());
+
+			// this.world = new SimWorld();
+		}
+
 	}
 
-    
-
-
-
-
-
-
-    //Input code ends here
+	// Input code ends here
 }
