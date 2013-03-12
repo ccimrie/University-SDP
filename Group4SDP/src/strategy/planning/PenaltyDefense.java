ebb5c56f1f3608f239ed2e05f5da63fdbfe9eeee
XapaJIaMnu <nheart@gmail.com>
2013-03-12 13:57:29 +0000

    Penaltydefense starts strategy after penalty kick

--- Group4SDP/src/strategy/planning/PenaltyDefense.java
+++ Group4SDP/src/strategy/planning/PenaltyDefense.java
@@ -2,11 +2,23 @@ package strategy.planning;
 
 import movement.RobotMover;
 import world.state.WorldState;
+import utility.SafeSleep;
+import world.state.Ball;
 
 public class PenaltyDefense extends StrategyInterface {
-
+	double originalx;
+	double originaly;
+	Ball ball;
+	
+	private Thread strategyThread;
+	private StrategyInterface strategy;
+	
 	public PenaltyDefense(WorldState world, RobotMover mover) {
 		super(world, mover);
+		//Original position of the ball. Use this to determine
+		this.ball = world.ball;
+		this.originalx = ball.x;
+		this.originaly = ball.y;
 	}
 
 	@Override
@@ -14,56 +26,60 @@ public class PenaltyDefense extends StrategyInterface {
 		double theirOriginal = Math.toDegrees(world.theirRobot.bearing);
 
 		int counter = 0;
+		while ((!shouldidie && !Strategy.alldie) &&
+				(Math.abs(ball.x - originalx) < 10) && 
+				((Math.abs(ball.y - originaly)) < 10)){
+			System.out.println(Math.toDegrees(world.theirRobot.bearing) + " "
+					+ theirOriginal);
 
-		System.out.println(Math.toDegrees(world.theirRobot.bearing) + " "
-				+ theirOriginal);
+			if (Math.toDegrees(world.theirRobot.bearing) > theirOriginal + 5
+					&& counter > -1) {
+				System.out.println("go backwards");
+				mover.move(0, -100);
+				try {
+					SafeSleep.sleep(600);
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				mover.stopRobot();
 
-		if (Math.toDegrees(world.theirRobot.bearing) > theirOriginal + 5
-				&& counter > -1) {
-			System.out.println("go backwards");
-			mover.move(0, -100);
-			try {
-				Thread.sleep(600);
-			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
+				theirOriginal = Math.toDegrees(world.theirRobot.bearing);
+				counter--;
+				try {
+					SafeSleep.sleep(600);
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
 			}
-			mover.stopRobot();
 
-			theirOriginal = Math.toDegrees(world.theirRobot.bearing);
-			counter--;
-			try {
-				Thread.sleep(600);
-			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
+			if (Math.toDegrees(world.theirRobot.bearing) < theirOriginal - 5
+					&& counter < 1) {
+				System.out.println("going forwards");
+
+				mover.move(0, 100);
+				try {
+					SafeSleep.sleep(600);;
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				mover.stopRobot();
+
+				theirOriginal = Math.toDegrees(world.theirRobot.bearing);
+				counter++;
+				try {
+					SafeSleep.sleep(600);
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
 			}
-			return;
+			System.out.println(counter);
 		}
-
-		if (Math.toDegrees(world.theirRobot.bearing) < theirOriginal - 5
-				&& counter < 1) {
-			System.out.println("going forwards");
-
-			mover.move(0, 100);
-			try {
-				Thread.sleep(600);
-			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-			mover.stopRobot();
-
-			theirOriginal = Math.toDegrees(world.theirRobot.bearing);
-			counter++;
-			try {
-				Thread.sleep(600);
-			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-			return;
-		}
-		System.out.println(counter);
+		strategy = new MainPlanner(world, mover);
+		strategyThread = new Thread(strategy);
+		strategyThread.start();
 	}
 }
