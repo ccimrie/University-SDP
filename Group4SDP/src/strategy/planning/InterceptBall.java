534cf38ce20dda61b4cd221ef05c10e621146d2a
Alex Adams <alex-j-a@hotmail.co.uk>
2013-03-19 19:54:25 +0000

    Hopefully improved interceptball

--- Group4SDP/src/strategy/planning/InterceptBall.java
+++ Group4SDP/src/strategy/planning/InterceptBall.java
@@ -1,16 +1,16 @@
 package strategy.planning;
 
 import movement.RobotMover;
+import strategy.calculations.DistanceCalculator;
+import strategy.movement.TurnToBall;
 import utility.SafeSleep;
 import vision.Position;
-import world.state.WorldState;
 import world.state.Ball;
 import world.state.Robot;
-import strategy.movement.TurnToBall;
-import strategy.planning.DribbleBall5;
+import world.state.WorldState;
 
 public class InterceptBall extends StrategyInterface {
-	private static final double distanceThreshold = 70;
+	private static final double distanceThreshold = 20;
 	private static final double angleThreshold = Math.toRadians(15);
 	private Ball ball;
 	private final double initx;
@@ -25,30 +25,28 @@ public class InterceptBall extends StrategyInterface {
 		initx = ball.x;
 		inity = ball.y;
 		us = world.getOurRobot();
-		//For use with the possession manager.
-		if(world.areWeBlue()){
+		// For use with the possession manager.
+		if (world.areWeBlue())
 			ourRobot = 1;
-		}
+		else
+			ourRobot = 2;
 	}
 
 	@Override
 	public void run() {
 		System.out.println("InterceptBall started");
-		/* This while loop waits until the other robot kicks the ball,
-		 * before going into intercept mode. Comment it out if you don't
-		 * want to use it.
+		/*
+		 * This while loop waits until the other robot kicks the ball, before
+		 * going into intercept mode. Comment it out if you don't want to use
+		 * it.
 		 */
-		/*double change = Math.abs(initx-ball.x) + Math.abs(inity-ball.y);
-		while (change < 10){
-			try {
-				SafeSleep.sleep(10);
-			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-			change = Math.abs(initx-ball.x) + Math.abs(inity-ball.y);
-		}
-		System.out.println("BALL MOVED! Intercept it!");
+		/*
+		 * double change = Math.abs(initx-ball.x) + Math.abs(inity-ball.y);
+		 * while (change < 10){ try { SafeSleep.sleep(10); } catch
+		 * (InterruptedException e) { // TODO Auto-generated catch block
+		 * e.printStackTrace(); } change = Math.abs(initx-ball.x) +
+		 * Math.abs(inity-ball.y); }
+		 * System.out.println("BALL MOVED! Intercept it!");
 		 */
 		double ballTurnAngle;
 
@@ -58,35 +56,40 @@ public class InterceptBall extends StrategyInterface {
 			while (!shouldidie && !Strategy.alldie) {
 				Position projBallPos = world.projectedBallPos();
 
-				if (world.distanceBetweenUsAndBall() > distanceThreshold) {
-					System.out.println("Projected ball pos: (" + projBallPos.getX() + ", " + projBallPos.getY() + ")");
+				if (DistanceCalculator.Distance(world.ourRobot.x,
+						world.ourRobot.y, projBallPos.getX(),
+						projBallPos.getY()) > distanceThreshold) {
+					System.out.println("Projected ball pos: ("
+							+ projBallPos.getX() + ", " + projBallPos.getY()
+							+ ")");
 					double targetX = (world.areWeOnLeft()) ? 60 : 560;
 					mover.moveTowards(targetX, projBallPos.getY());
 				}
-				else {
-					break;
-				}
 				SafeSleep.sleep(50);
+
+				// Proceed to dribble if we have the ball
+				if (world.whoHasTheBall() == ourRobot)
+					break;
 			}
 			System.out.println("Exited intercept part");
-			//In case we don't intercept the ball in time, ie it hits us on
-			//the side, start dribbleball5 from the previous milestone
-			//immediately.
+			// In case we don't intercept the ball in time, ie it hits us on
+			// the side, start dribbleball5 from the previous milestone
+			// immediately.
 
-			//Check if the ball is closer to our goal then us.
-			if(Math.abs(ball.x - world.getOurGoal().getX()) >
-			Math.abs(us.x - world.getOurGoal().getX())){
+			// Check if the ball is closer to our goal then us.
+			if (Math.abs(ball.x - world.getOurGoal().getX()) > Math.abs(us.x
+					- world.getOurGoal().getX())) {
 				dribble.dribbleBall(world, mover);
-			}else{
+			} else {
 
 				// After we are on the trajectory, rotate to face the ball.
 				// and start moving towards it.
 				ballTurnAngle = Math.toRadians(TurnToBall.Turner(us, ball));
 				mover.rotate(ballTurnAngle);
 				mover.waitForCompletion();
-				mover.move(0,100);
-				while(world.whoHasTheBall() != ourRobot &&
-						!shouldidie && !Strategy.alldie){
+				mover.move(0, 100);
+				while (world.whoHasTheBall() != ourRobot && !shouldidie
+						&& !Strategy.alldie) {
 					SafeSleep.sleep(50);
 				}
 				mover.stopRobot();
